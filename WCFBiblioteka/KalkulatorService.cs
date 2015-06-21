975927f4eb1229227d23d2dd8e61a28d1ceed353
monia500 <monia500@onet.eu>
2015-06-21 20:47:41 +0200

    PierwszyCommit

---
+++ WCFBiblioteka/KalkulatorService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFBiblioteka
+{
+    public class KalkulatorService : IKalkulator
+    {
+
+        public int dodaj(int wartosc1, int wartosc2)
+        {
+            int wynik = wartosc1 + wartosc2;
+            return wynik; 
+        }
+    }
+}
